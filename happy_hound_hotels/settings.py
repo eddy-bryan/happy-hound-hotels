a9ead9d9b79456b2528333dc576eed2caae44464
Edward Bryan <eddy-bryan@hotmail.co.uk>
2024-03-24 15:37:44 +0000

    Added code to test homepage view

--- happy_hound_hotels/settings.py
+++ happy_hound_hotels/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_q#fwj*yyc)3ltr1@1x^u9ky)tczq!(u&*_*@!6aq)yvvl9d1!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "8000-eddybryan-happyhoundhot-zw0te6aqijt.ws-eu110.gitpod.io",
+    ".herokuapp.com"
+]
 
 
 # Application definition
@@ -37,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'kennel_manager',
+    'account_management',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +83,7 @@ WSGI_APPLICATION = 'happy_hound_hotels.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
--- kennel_manager/views.py
+++ kennel_manager/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("Test")
+
+
+# def search_results():
+
+
+# def kennel_details():
