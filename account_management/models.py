da9dc089ef123fb7ac1ccca648a4a3556ead4c11
Edward Bryan <eddy-bryan@hotmail.co.uk>
2024-03-25 21:20:44 +0000

    Created the PetProfile model

--- account_management/models.py
+++ account_management/models.py
@@ -10,3 +10,18 @@ class UserProfile(models.Model):
     county = models.CharField(max_length=50)
     postal_code = models.CharField(max_length=10)
     contact_number = models.CharField(max_length=11, unique=True)
+
+class PetProfile(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    pet_name = models.CharField(max_length=50)
+    breed = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    # vaccination_certificate = CloudinaryField('vaccination_details', null=True, blank=True)
+    vet_name = models.CharField(max_length=50)
+    vet_address_line_1 = models.CharField(max_length=50)
+    vet_address_line_2 = models.CharField(max_length=50, blank=True)
+    city = models.CharField(max_length=50)
+    county = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=10)
+    contact_number = models.CharField(max_length=11)
+    
