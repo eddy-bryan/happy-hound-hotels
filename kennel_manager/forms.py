719b717b1d6ed0310f33ffe4897691456c691c43
Edward Bryan <eddy-bryan@hotmail.co.uk>
2024-03-28 19:33:04 +0000

    Added kennel search form

---
+++ kennel_manager/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+
+class SearchForm(forms.Form):
+    check_in_date = forms.DateField(label='Check-in Date')
+    check_out_date = forms.DateField(label='Check-out Date')
+    num_pets = forms.IntegerField(min_value=0, label='Number of Pets')
--- kennel_manager/views.py
+++ kennel_manager/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Kennel
+from .forms import SearchForm
 
 def home(request):
-    return render(request, 'kennel_manager/index.html')
+    if request.method == 'GET':
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            check_in_date = form.cleaned_data['check_in_date']
+            check_out_date = form.cleaned_data['check_out_date']
+            num_pets = form.cleaned_data['num_pets']
+            # Render the kennel_list.html template only if the form is valid
+            return render(request, 'kennel_manager/kennel_list.html', {'form': form})
+    else:
+        form = SearchForm()
+    return render(request, 'kennel_manager/index.html', {'form': form})
 
 
 class KennelList(generic.ListView):
