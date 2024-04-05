9c154348825e40b49c4c5abe15255faa8303ab9f
Edward Bryan <eddy-bryan@hotmail.co.uk>
2024-04-05 18:00:33 +0000

    Updated kennel list template and styling

--- static/js/script.js
+++ static/js/script.js
@@ -25,3 +25,19 @@ $(function() {
         minDate: 2  // Initial minimum date is 2 days from today
     }).attr('readonly', 'readonly');
 });
+
+$(function() {
+    $("form").submit(function(event) {
+        var checkInDate = $("#id_check_in_date").val();
+        var checkOutDate = $("#id_check_out_date").val();
+
+        if (!checkInDate) {
+            event.preventDefault();
+            $("#check-in-error").removeClass("d-none");
+        }
+        if (!checkOutDate) {
+            event.preventDefault();
+            $("#check-out-error").removeClass("d-none");
+        }
+    });
+});
