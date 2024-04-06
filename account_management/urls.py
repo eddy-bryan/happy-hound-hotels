f129ad5eb668ae8e21871c70638862a1ecb6dbab
Edward Bryan <eddy-bryan@hotmail.co.uk>
2024-04-06 18:18:35 +0000

    Updated url patterns to provide a unique url for pet profiles

--- account_management/urls.py
+++ account_management/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('user_profile/', views.user_profile, name='user_profile'),
+    path('user_profile', views.user_profile, name='user_profile'),
+    path('<str:owner>/<str:pet_name>/', views.pet_profile, name='pet_profile'),
 ]
--- account_management/views.py
+++ account_management/views.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import User
 from .models import UserProfile, PetProfile
 
 def user_profile(request):
-    # Retrieve the UserProfile object for the current user
     user_profile = UserProfile.objects.get(username=request.user)
-    
-    # Filter PetProfile objects based on the current user
     user_pets = PetProfile.objects.filter(owner=request.user)
-    
-    # Pass the UserProfile and user_pets objects to the template context
     context = {'user_profile': user_profile, 'user_pets': user_pets}
-    
-    # Render the template with the context
     return render(request, 'account_management/user_profile.html', context)
+
+
+def pet_profile(request, owner, pet_name):
+    pet_name = PetProfile.objects.get(pet_name=pet_name)
+    owner = UserProfile.objects.get(username=request.user)
+    context = {'pet_name': pet_name, 'owner': owner}
+    return render(request, 'account_management/pet_profile.html', context)
