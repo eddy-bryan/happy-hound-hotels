37e433be1c17f908f2a80d0be9efe60a0fbf9294
Edward Bryan <eddy-bryan@hotmail.co.uk>
2024-04-08 18:32:37 +0000

    Turned on debug to resolve deployment error

--- happy_hound_hotels/settings.py
+++ happy_hound_hotels/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-_q#fwj*yyc)3ltr1@1x^u9ky)tczq!(u&*_*@!6aq)yvvl9d1!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "8000-eddybryan-happyhoundhot-zw0te6aqijt.ws-eu110.gitpod.io",
